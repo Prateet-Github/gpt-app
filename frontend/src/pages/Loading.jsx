a7c68c601c79c5f417b4b9467da33bccc1a229cd
Prateet Tiwari <prateettiwari29@gmail.com>
2025-08-26 01:43:53 +0530

    loading page done

--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { use, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      navigate("/");
+    }, 3000);
+    return () => clearTimeout(timeOut);
+  }, []);
+
+  return (
+    <div className="bg-gradient-to-b from-[#531B81] to-[#29184B] backdrop-opacity-60 flex items-center justify-center h-screen w-screen text-white text-2xl">
+      <div className="w-10 h-10 rounded-full border-3 border-white border-t-transparent animate-spin"></div>
+    </div>
+  );
 };
 
 export default Loading;
