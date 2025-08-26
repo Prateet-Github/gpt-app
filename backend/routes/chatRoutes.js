1c9c539cbed3ad0310eed34ef3949fba89be83b3
Prateet Tiwari <prateettiwari29@gmail.com>
2025-08-26 21:31:06 +0530

    text chat res done image res causing issue will debug later

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { protect } from '../middlewares/auth.js';
+import { createChat } from '../controllers/chatController.js';
+import { getChats } from '../controllers/chatController.js';
+import { deleteChat } from '../controllers/chatController.js';
+
+const chatRouter = express.Router();
+
+chatRouter.get('/create',protect, createChat)
+chatRouter.get('/get',protect, getChats)
+chatRouter.delete('/delete',protect, deleteChat)
+
+export default chatRouter;
